02d79b333297290df35097db7d91f2dc94d77d91
NITHIN.C <71306445+niths1271@users.noreply.github.com>
2023-04-08 10:24:27 +0530

    changes

--- material-react-app/src/App.js
+++ material-react-app/src/App.js
@@ -17,11 +17,9 @@ import Configurator from "examples/Configurator";
 
 // Material Dashboard 2 React themes
 import theme from "assets/theme";
-import themeRTL from "assets/theme/theme-rtl";
 
 // Material Dashboard 2 React Dark Mode themes
 import themeDark from "assets/theme-dark";
-import themeDarkRTL from "assets/theme-dark/theme-rtl";
 
 // RTL plugins
 import rtlPlugin from "stylis-plugin-rtl";
